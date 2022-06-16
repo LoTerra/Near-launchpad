741dba39eae2937e70c67e885cf9c127b712a64c
0xantman <antho74830@gmail.com>
2022-06-16 20:30:33 +0200

    Added UnorederedMap type with account id and whitelist, fix and test

--- src/lib.rs
+++ src/lib.rs
@@ -1,101 +1,122 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{AccountId, env, log, near_bindgen, require, Timestamp};
+use near_sdk::collections::UnorderedMap;
 use near_sdk::json_types::U128;
-use near_contract_standards;
-
+use near_sdk::{env, log, near_bindgen, require, AccountId, Timestamp};
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Launchpad {
     // See more data types at https://doc.rust-lang.org/book/ch03-02-data-types.html
-    whitelist: Vec<WhitelistState>,
-    minting_price: u128,
+    whitelist: UnorderedMap<AccountId, WhitelistState>,
+    minting_price: U128,
     admin: AccountId,
     usdc_account_id: AccountId,
     usdt_account_id: AccountId,
     dai_account_id: AccountId,
     private_sale_start: u64,
-    public_sale_start: u64
+    public_sale_start: u64,
 }
 
 #[derive(BorshDeserialize, BorshSerialize, Debug)]
 pub struct WhitelistState {
     // See more data types at https://doc.rust-lang.org/book/ch03-02-data-types.html
-    account_id: String,
     restricted: bool,
     minting_start: Timestamp,
-    minting_price: U128
+    minting_price: U128,
 }
-
 
 #[near_bindgen]
 impl Launchpad {
-
     #[init]
-    pub fn new(minting_price: u128, usdc_account_id: AccountId, usdt_account_id: AccountId, dai_account_id: AccountId, private_sale_start: u64, public_sale_start: u64) -> Self {
+    pub fn new(
+        minting_price: U128,
+        usdc_account_id: AccountId,
+        usdt_account_id: AccountId,
+        dai_account_id: AccountId,
+        private_sale_start: u64,
+        public_sale_start: u64,
+    ) -> Self {
         log!("Custom counter initialization!");
-        require!(private_sale_start < public_sale_start, "The private sale should start before the public sale");
-        Self { whitelist: vec![], minting_price, admin: env::signer_account_id(), usdc_account_id, usdt_account_id, dai_account_id, private_sale_start , public_sale_start }
+        require!(
+            private_sale_start < public_sale_start,
+            "The private sale should start before the public sale"
+        );
+        Self {
+            whitelist: UnorderedMap::new(b"s"),
+            minting_price,
+            admin: env::signer_account_id(),
+            usdc_account_id,
+            usdt_account_id,
+            dai_account_id,
+            private_sale_start,
+            public_sale_start,
+        }
     }
 
-    pub fn add_whitelist(&mut self, minting_start: Timestamp, minting_price: U128) {
+    pub fn add_whitelist(&mut self, address: String, minting_start: Timestamp, minting_price: U128) {
         require!(env::signer_account_id() == self.admin, "Owner's method");
 
         // https://doc.rust-lang.org/std/primitive.i8.html#method.wrapping_add
-        self.whitelist.push( WhitelistState{
-            account_id: env::signer_account_id().to_string(),
+        self.whitelist.insert(&AccountId::new_unchecked(address), &WhitelistState {
             restricted: false,
             minting_start,
             minting_price
         });
 
-        log!(format!("Whitelist user {}", env::signer_account_id()).as_str());
 
+        log!(format!("Whitelist user {}", env::signer_account_id()).as_str());
     }
 
-    pub fn get_whitelist(&self) -> &Vec<WhitelistState> {
-        let data = &self.whitelist;
-        return data;
+    pub fn get_whitelist(&self, from_index: u64, limit: u64) -> Vec<(AccountId, WhitelistState)> {
+        let keys = self.whitelist.keys_as_vector();
+        let values = self.whitelist.values_as_vector();
+        (from_index..std::cmp::min(from_index + limit, self.whitelist.len()))
+            .map(|index| (keys.get(index).unwrap(), values.get(index).unwrap()))
+            .collect()
     }
 
     pub fn callback_buy(&self, sender: AccountId, amount: U128) {
         // Authorized only by supported FT contract
-        require!(self.dai_account_id == env::predecessor_account_id() || self.usdc_account_id == env::predecessor_account_id() || self.usdt_account_id == env::predecessor_account_id(), "Only allowed NF contracts can call this message");
+        require!(
+            self.dai_account_id == env::predecessor_account_id()
+                || self.usdc_account_id == env::predecessor_account_id()
+                || self.usdt_account_id == env::predecessor_account_id(),
+            "Only allowed NF contracts can call this message"
+        );
         // Verify the amount sent match with minting cost
-        require!(amount == self.minting_price, format!("Wrong amount sent, minting price {} DAI/USDC/USDT", self.minting_price));
+        require!(
+            amount == self.minting_price,
+            format!(
+                "Wrong amount sent, minting price {:?} DAI/USDC/USDT",
+                self.minting_price
+            )
+        );
 
         // TODO: Verify if minting time have started
-        match env::block_timestamp() {
-            Some(time) if self.public_sale_start > time => {},
-            Some(time) if self.private_sale_start > time => {}
-            _ => panic!()
-        }
+        // match env::block_timestamp() {
+        //     Some(time) if self.public_sale_start > time => {}
+        //     Some(time) if self.private_sale_start > time => {}
+        //     _ => panic!(),
+        // }
 
-        if self.private_sale_start < env::block_timestamp() {
-
-        }
+        if self.private_sale_start < env::block_timestamp() {}
 
         /*
-            TODO : If private sale started verify if sender is into whitelist
-         */
+           TODO : If private sale started verify if sender is into whitelist
+        */
         // TODO verify minting limit
-
-
 
         // Finally
         // TODO mint an NFT pack and send it to the sender
     }
-
 }
-
 
 #[cfg(test)]
 #[cfg(all(test, not(target_arch = "wasm32")))]
 mod tests {
     use super::*;
     use near_sdk::test_utils::VMContextBuilder;
-    use near_sdk::{AccountId, testing_env, VMContext};
-    use std::convert::TryInto;
+    use near_sdk::{testing_env, AccountId, VMContext};
 
     fn get_context(is_view: bool) -> VMContext {
         VMContextBuilder::new()
@@ -104,28 +125,35 @@ mod tests {
             .build()
     }
 
-    fn default_launchpad_init(admin: String) -> Launchpad{
-        Launchpad{ whitelist: vec![], minting_price: 100, admin: AccountId::new_unchecked(admin), usdc_account_id: AccountId::new_unchecked("usdc_near".to_string()), usdt_account_id: AccountId::new_unchecked("usdt_near".to_string()), dai_account_id: AccountId::new_unchecked("dai_near".to_string()) }
+    fn default_launchpad_init(admin: String) -> Launchpad {
+        self::Launchpad::new(
+            U128::from(100),
+            AccountId::new_unchecked("usdc_near".to_string()),
+            AccountId::new_unchecked("usdt_near".to_string()),
+            AccountId::new_unchecked("dai_near".to_string()),
+            100,
+            200,
+        )
     }
 
     #[test]
-    fn my_test() {
+    fn try_whitelist() {
         let mut context = get_context(false);
         context.signer_account_id = AccountId::new_unchecked("admin_near".to_string());
         testing_env!(context);
 
         let mut contract = default_launchpad_init("admin_near".to_string());
-        contract.add_whitelist(env::block_timestamp().saturating_add(100), U128::from(1000));
-        println!("Ok: {:?}", contract.get_whitelist());
+        contract.add_whitelist("alice_near".to_string(), env::block_timestamp().saturating_add(100), U128::from(1000));
+        println!("Ok: {:?}", contract.get_whitelist(0, 10));
     }
 
     #[test]
     #[should_panic]
-    fn should_fail(){
+    fn should_fail() {
         let context = get_context(false);
         testing_env!(context);
         let mut contract = default_launchpad_init("admin_near".to_string());
-        contract.add_whitelist(env::block_timestamp().saturating_add(100), U128::from(10));
+        contract.add_whitelist("alice_near".to_string(), env::block_timestamp().saturating_add(100), U128::from(10));
     }
 
     #[test]
@@ -135,7 +163,7 @@ mod tests {
         testing_env!(context);
 
         let mut contract = default_launchpad_init("admin_near".to_string());
-        contract.buy();
+        contract.callback_buy(AccountId::new_unchecked("usdc_near".to_string()), U128(100));
         //println!("Ok: {:?}", contract.get_whitelist());
     }
 }
